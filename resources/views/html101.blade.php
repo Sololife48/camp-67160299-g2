4cacef710fea3ad5d7d8712e9fe69a1fb09a7fab
Sololife <solofake48@gmail.com>
2025-12-01 16:47:55 +0700

    PALM

---
+++ resources/views/html101.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title> Page Title</title>
+        <link rel="stylesheet" href="styles.css" />
+    </head>
+    <body>
+        <h1> ภาษาไทย </h1>
+        <p> style="color:blue;"> This is paragraph. </p>
+    </body>
+    </html>
